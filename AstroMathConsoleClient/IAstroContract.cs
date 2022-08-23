b8eb4f1f5bfb2e09597a54c33c3e1b09ce52052e
James Boyd <jamesramsayboyd@gmail.com>
2022-08-23 10:07:25 +0800

    Added simple console client application

---
+++ AstroMathConsoleClient/IAstroContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace AstroMath
+{
+    [ServiceContract]
+    public interface IAstroContract
+    {
+        [OperationContract]
+        double StarVelocity(double a, double b);
+
+        [OperationContract]
+        double StarDistance(double a);
+
+        [OperationContract]
+        double TemperatureInKelvin(double a);
+
+        [OperationContract]
+        double EventHorizon(double a);
+    }
+}
---
+++ AstroMathConsoleClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace AstroMath
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("### AstroMath Client Started ###");
+
+            ChannelFactory<IAstroContract> pipeFactory =
+              new ChannelFactory<IAstroContract>(
+                new NetNamedPipeBinding(),
+                  new EndpointAddress("net.pipe://localhost/PipeReverse"));
+
+            IAstroContract astroPipeProxy = pipeFactory.CreateChannel();
+
+            while (true)
+            {
+                Console.WriteLine("### Star Velocity ###");
+                Console.WriteLine("Observed Wavelength: ");
+                double ObservedWavelength = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Rest Wavelength: ");
+                double RestWavelength = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Star Velocity = " + astroPipeProxy.StarVelocity(ObservedWavelength, RestWavelength) + " m/s");
+                Console.WriteLine();
+                Console.WriteLine("### Star Distance ###");
+                Console.WriteLine("Parallax Angle: ");
+                double P = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Star Distance = " + astroPipeProxy.StarDistance(P) + " parsecs");
+                Console.WriteLine();
+                Console.WriteLine("### Celsius/Kelvin Conversion ###");
+                Console.WriteLine("Temperature in Celsius: ");
+                double C = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Temperature in Kelvin = " + astroPipeProxy.TemperatureInKelvin(C) + "K");
+                Console.WriteLine();
+                Console.WriteLine("### Event Horizon ###");
+                Console.WriteLine("Schwarzschild Radius: ");
+                double R = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Event Horizon = " + astroPipeProxy.EventHorizon(R) + "m");
+                Console.WriteLine();
+            }
+        }
+    }
+}
